78db6a1caa1463b2da7f7738362785f7997df2d1
andreichenko256 <andreichenko.kirill2000@gmail.com>
2021-03-29 21:08:36 +0300

    Added CollectionViewController

---
+++ lab-1.1/controllers/CollectionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewController.swift
+//  lab-1.1
+//
+//  Created by Kirill on 29.03.2021.
+//
+
+import UIKit
+
+class CollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+}
